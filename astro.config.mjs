7294c46aa9a7fb9a7faa7e41c03b9375fe6d0b34
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2024-04-29 11:06:33 +0100

    doc: setup pages for all Disciplines

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,163 @@ export default defineConfig({
           items: [
             { label: "Installation", link: "/surveilr/installation" },
             {
+              label: "Disciplines and WPAs",
+              items: [
+                {
+                  label: "Software Engineers",
+                  link: "/surveilr/disciplines/software-engineer",
+                },
+                {
+                  label: "System Engineers",
+                  link: "/surveilr/disciplines/system-engineer",
+                },
+                {
+                  label: "QA Engineers",
+                  link: "/surveilr/disciplines/qa",
+                },
+                {
+                  label: "Security Analysts/Engineers",
+                  link: "/surveilr/disciplines/security-analyst",
+                },
+                {
+                  label: "Compliance Officers",
+                  link: "/surveilr/disciplines/compliance-officer",
+                },
+                {
+                  label: "Regulatory Affairs Specialists",
+                  link: "/surveilr/disciplines/regulatory-affairs",
+                },
+                {
+                  label: "Project Managers",
+                  link: "/surveilr/disciplines/project-manager",
+                },
+                {
+                  label: "Configuration Management Specialists",
+                  link: "/surveilr/disciplines/configuration",
+                },
+                {
+                  label: "Database Administrators",
+                  link: "/surveilr/disciplines/database-admin",
+                },
+                {
+                  label: "Network Administrators",
+                  link: "/surveilr/disciplines/network-admin",
+                },
+                {
+                  label: "DevOps Engineers",
+                  link: "/surveilr/disciplines/devops",
+                },
+                {
+                  label: "Technical Writers",
+                  link: "/surveilr/disciplines/technical-writer",
+                },
+                {
+                  label: "UI/UX Designers",
+                  link: "/surveilr/disciplines/user-experience",
+                },
+                {
+                  label: "Legal Counsel",
+                  link: "/surveilr/disciplines/legal-counsel",
+                },
+                {
+                  label: "Subject Matter Experts",
+                  link: "/surveilr/disciplines/subject-matter-expert",
+                },
+                {
+                  label: "Customer Support Representatives",
+                  link: "/surveilr/disciplines/customer-support",
+                },
+                {
+                  label: "Training and Education Specialists",
+                  link: "/surveilr/disciplines/training",
+                },
+                {
+                  label: "Risk Management Specialists",
+                  link: "/surveilr/disciplines/risk-management",
+                },
+                {
+                  label: "Incident Response Team Members",
+                  link: "/surveilr/disciplines/incident-response",
+                },
+                {
+                  label: "Performance and Reliability Engineers",
+                  link: "/surveilr/disciplines/performance-reliability",
+                },
+                {
+                  label: "Procurement Specialists",
+                  link: "/surveilr/disciplines/procurement-specialist",
+                },
+                {
+                  label: "Auditors",
+                  link: "/surveilr/disciplines/auditors",
+                },
+                {
+                  label: "External Assessors",
+                  link: "/surveilr/disciplines/external-assessor",
+                },
+                {
+                  label: "Ethical Hackers",
+                  link: "/surveilr/disciplines/ethical-hacker",
+                },
+                {
+                  label: "Penetration Testers",
+                  link: "/surveilr/disciplines/penetration-tester",
+                },
+                {
+                  label: "Compliance Automation Specialists",
+                  link: "/surveilr/disciplines/compliance-automation",
+                },
+                {
+                  label: "Business Analysts",
+                  link: "/surveilr/disciplines/business-analyst",
+                },
+                {
+                  label: "Product Owners",
+                  link: "/surveilr/disciplines/product-owner",
+                },
+                {
+                  label: "Product Managers",
+                  link: "/surveilr/disciplines/product-managers",
+                },
+                {
+                  label: "Human Factors Engineers",
+                  link: "/surveilr/disciplines/human-factors-engineers",
+                },
+                {
+                  label: "System Integrators",
+                  link: "/surveilr/disciplines/system-integrators",
+                },
+                {
+                  label: "Change Management Specialists",
+                  link: "/surveilr/disciplines/change-management",
+                },
+                {
+                  label: "Legal and Regulatory Consultants",
+                  link: "/surveilr/disciplines/legal-consultants",
+                },
+                {
+                  label: "Supply Chain Managers",
+                  link: "/surveilr/disciplines/supply-chain-managers",
+                },
+                {
+                  label: "Facilities Managers",
+                  link: "/surveilr/disciplines/facilities-managers",
+                },
+                {
+                  label: "Medical Device Specialists",
+                  link: "/surveilr/disciplines/medical-device-specialists",
+                },
+                {
+                  label: "Aerospace Engineers",
+                  link: "/surveilr/disciplines/aerospace-engineers",
+                },
+                {
+                  label: "Automotive Engineers",
+                  link: "/surveilr/disciplines/automotive-engineers",
+                },
+              ],
+            },
+            {
               label: "Ingestion",
               items: [
                 { label: "Files Ingestion", link: "/surveilr/ingest/files" },
---
+++ src/content/docs/surveilr/disciplines/qa.md
@@ -0,0 +1,4 @@
+---
+title: Quality Assurance Engineers
+description: explanation on how quality assurance engineers make use of surveilr.
+---
